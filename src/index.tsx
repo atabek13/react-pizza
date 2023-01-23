d229ce0a7147904a8e5a1a357379f942270296df
Atabek Otepbergenov <atabek122002@gmail.com>
2023-01-23 17:21:44 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import 'react-toastify/dist/ReactToastify.css';
-import 'macro-css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
+import "macro-css";
 
-import { store } from './store';
+import { store } from "./store";
 
-import App from './App';
+import App from "./App";
 
-import './styles/index.scss';
+import "./styles/index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </Router>,
+  </Router>
 );
